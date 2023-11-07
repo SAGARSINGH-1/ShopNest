83d84ec0a3af8871d1ed5a2130cba960b53e343c
Sagar Singh <sagar.01ko@gmail.com>
2023-11-07 11:16:45 +0530

    Updated Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <Navbar className=''>
+        <div>
+            <img src="/public/img/download-removebg-preview.png" alt="company logo"/>
+        </div>
+        <div>
+            <ul>
+                <li>MEN</li>
+                <li>WOMEN</li>
+                <li>KIDS</li>
+                <li>HOME & LIVING</li>
+                <li>BEAUTY</li>
+                <li>STUDIO</li>
+            </ul>
+        </div>
+    </Navbar>
+  )
+}
+
+export default Navbar
